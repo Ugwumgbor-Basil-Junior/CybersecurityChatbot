0cf4a9333257d7ccc1b4b491c6e0a980a53c3bc3
Ugwumgbor Basil <ST10482983@imconnect.edu.za>
2026-03-31 19:06:09 +0200

    Integrate Chatbot into Program entry point

--- CybersecurityChatbot/Program.cs
+++ CybersecurityChatbot/Program.cs
@@ -7,7 +7,7 @@ namespace CybersecurityChatbot
 {
     class Program
     {
-        static void Main(string[] args)
+ static void Main(string[] args)
         {
             Console.Title = "Cybersecurity Awareness Bot";
             Console.WindowWidth = 100;
